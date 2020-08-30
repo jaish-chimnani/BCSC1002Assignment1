167c3b788330c4ecdfc674ccb1820e2b415b257e
Mr.Chimnani <jaish.chimnani_cs19@gla.ac.in>
2020-08-30 13:21:04 +0530

    creating constructor

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,4 +11,10 @@ public class Book {
     private String bookName;
     private String authorName;
     private String bookISBNNumber;
+
+    public Book(String bookName, String authorName, String bookISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.bookISBNNumber = bookISBNNumber;
+    }
 }
