d594fad2bc9e18dff8639f965b0556c67b111b62
Mr.Chimnani <jaish.chimnani_cs19@gla.ac.in>
2020-08-30 14:23:27 +0530

    introducing getter and setter methods

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -25,4 +25,11 @@ public class Student{
      *
      * @return the name of the student (first name, middle name, last name- all String).
      */
+    public String[] getStudentName() {
+        return studentName;
+    }
+
+    public void setNameOfStudent(String[] nameOfStudent) {
+        this.studentName = studentName;
+    }
 }
